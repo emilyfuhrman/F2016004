efe698be90b26c02604e70f56c7b019f0f4fcf54
emilyfuhrman <emily.c.fuhrman@gmail.com>
2016-05-19 09:42:25 -0400

    setup

--- js/vis.js
+++ js/vis.js
@@ -1,22 +1,24 @@
-/*window.onload = function(){
+window.onload = function(){
 
 	return {
 
+		data:null,
+
+		getData:function(_callback){
+			d3.csv('data/buildings.csv',function(e,d){
+				if(!e){ 
+					self.data = d; 
+					_callback();
+				}
+			});
+		},
 		generate:function(){
+			var self = this;
+			self.getData(self.draw);
+		},
+		draw:function(){
+			var self = this;
 			debugger;
 		}
 	}
-}().generate();*/
-
-var init = function(){
-
-	return {
-
-		generate:function(){
-			debugger;
-		}
-	}
-}
-
-var vis = init();
-vis.generate();
+}().generate();
